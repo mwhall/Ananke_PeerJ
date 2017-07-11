adf198c92ab95b137d53759aec6c8ed715fa9c25
Michael Hall <hallm2533@gmail.com>
2017-07-11 17:01:41 -0300

    Minor updates to Ananke scripts for Mendota analysis

--- McMahon_Mendota/Ananke_all_sequences.sh
+++ McMahon_Mendota/Ananke_all_sequences.sh
@@ -19,6 +19,7 @@ ananke tabulate -i sequence_data/seq.fasta -o Mendota_Ananke_full.h5 -f seq.uniq
 
 #Save some space, gzip this FASTA file
 gzip sequence_data/seq.fasta
+
 #Use an abundance filter
 ananke filter -i Mendota_Ananke_full.h5 -o Mendota_Ananke_full_filtered.h5 -f abundance -t 100
 
@@ -32,11 +33,12 @@ usearch -sortbysize seq.unique.full.min100.fasta -output seq.unique.sorted.fasta
 usearch -cluster_otus seq.unique.sorted.fasta -otus otus.fasta -otu_radius_pct 3
 usearch -usearch_global seq.unique.full.min100.fasta -db otus.fasta -strand both -id 0.97 -uc map.uc -threads 6
 #Script found at: https://github.com/neufeld/MESaS
-python2 /home/mwhall/Software/MESaS/scripts/mesas-uc2clust -t 6 map.uc seq_otus.txt
+python2 mesas-uc2clust -t 6 map.uc seq_otus.txt
 sed -i 's/;size=[0-9]*;//g' seq_otus.txt
 awk 'BEGIN{i=0;}{print i "\t" $0; i+=1;}' seq_otus.txt > seq_renumbered.txt
 ananke add sequence_clusters -i Mendota_Ananke_full_filtered.h5 -d seq_renumbered.txt
 
 #Optional: Add taxonomy information
 #Edit TaxAss.sh for the full run to create taxonomy_full.txt from scratch, if desired
+#Note that taxonomy.txt and taxonomy_full.txt should be in the GitHub
 ananke add taxonomy -i Mendota_Ananke_full_filtered.h5 -d TaxAss/taxonomy_full.txt
